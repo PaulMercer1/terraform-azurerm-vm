787c3824fdce2ac46ebd64773c07e41f6f6d520d
PaulMercer1 <48514185+PaulMercer1@users.noreply.github.com>
2023-05-10 16:46:19 +0100

    Update main.tf
    Upgraded Ubuntu version

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "20_04-lts-gen2"
     version   = "latest"
   }
 }
